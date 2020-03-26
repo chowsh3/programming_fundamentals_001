8c99af4174a3183f280b8fc9c644d4406b26030f
chowsh3 <shobir.c@icloud.com>
2020-03-26 00:24:45 +0000

    Updated

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,33 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+function makeHalfPrice (price) {
+    return (price * 0.5);
+}
+
+function countBooks (booksList) {
+  return booksList.length;
+}
+
+function isInStock (book) {
+        if(book.quantity <= 0) { 
+        return false;
+        } 
+    return true;
+}
+
+function getTotalOrderPrice (price,quantity) {
+    return (price * quantity)*1.2;
+}
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,45 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+  
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+});
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 50 when passed 100", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns total number of books",() => {
+    expect(utils.countBooks(["1", "2", "3"])).toBe(3);
+  });
+});
+
+
+describe("utils.isInStock", () => {
+  test("returns false if is in stock", () => {
+  const book = {
+    title: "Crushing It",
+    author: "Gary Vaynerchuk",
+    yearOfPublication: 2018,
+    quantity: 10
+  };
+    expect(utils.isInStock(book)).toBe(true);
+ });
+});
+
+describe("utils.getTotalOrderPrice", () => {
+  test("returns total order price", () => {
+    expect(utils.getTotalOrderPrice(8,10)).toBe(96);
+  });
 });
