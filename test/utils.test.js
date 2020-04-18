de675ddbea533e1593aab2872f40d8a7d5250a22
chowsh3 <shobir.c@icloud.com>
2020-04-19 00:36:10 +0100

    fixed typo

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,11 +4,11 @@ describe("utils.addVAT", () => {
   test("returns 36 when passed 30", () => {
     expect(utils.addVAT(30)).toBe(36);
   });
-  
+
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
-  
+
   test("returns 118.8 when passed 99", () => {
     expect(utils.addVAT(99)).toBe(118.8);
   });
@@ -27,26 +27,26 @@ describe("utils.makeHalfPrice", () => {
 });
 
 describe("utils.countBooks", () => {
-  test("returns total number of books",() => {
+  test("returns total number of books", () => {
     expect(utils.countBooks(["1", "2", "3"])).toBe(3);
   });
 });
 
 
 describe("utils.isInStock", () => {
-  test("returns false if is in stock", () => {
-  const book = {
-    title: "Crushing It",
-    author: "Gary Vaynerchuk",
-    yearOfPublication: 2018,
-    quantity: 10
-  };
+  test("returns true if is in stock", () => {
+    const book = {
+      title: "Crushing It",
+      author: "Gary Vaynerchuk",
+      yearOfPublication: 2018,
+      quantity: 10
+    };
     expect(utils.isInStock(book)).toBe(true);
- });
+  });
 });
 
 describe("utils.getTotalOrderPrice", () => {
   test("returns total order price", () => {
-    expect(utils.getTotalOrderPrice(8,10)).toBe(96);
+    expect(utils.getTotalOrderPrice(8, 10)).toBe(96);
   });
 });
